b966b0ce4c58851ff4b30428171a7ac2eb8bb9fb
hin <>
2024-05-25 18:24:39 +0800

    force allow cros

--- asset-transfer-basic/rest-api-typescript/src/server.ts
+++ asset-transfer-basic/rest-api-typescript/src/server.ts
@@ -49,8 +49,12 @@ export const createServer = async (): Promise<Application> => {
   // initialize passport js
   app.use(passport.initialize());
 
+  console.info('process.env.NODE_ENV', process.env.NODE_ENV);
+  console.info('force enable cors for development');
   if (process.env.NODE_ENV === 'development') {
-    app.use(cors());
+    app.use(cors({ origin: '*' }));
+  } else {
+    app.use(cors({ origin: '*' }));
   }
 
   if (process.env.NODE_ENV === 'test') {
